774be86531997543f418f1befef010416f63ac3d
CharlotteLanguages <121199846+CharlotteLanguages@users.noreply.github.com>
2023-07-13 16:35:27 +0200

    Ajustes login y register

--- routes/register_routers.js
+++ routes/register_routers.js
@@ -4,7 +4,7 @@ const routes = express.Router()
 routes.get('/', (req, res) =>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
-        conn.query('SELECT * FROM idRole', (err, rows)=>{
+        conn.query('SELECT * FROM PERSON', (err, rows)=>{
                     if(err) return res.send(err)
                     res.json(rows)
                 })
@@ -13,7 +13,7 @@ routes.get('/', (req, res) =>{
     routes.get('/:id', (req, res) =>{
         req.getConnection((err, conn)=>{
             if(err) return res.send(err)
-            conn.query('SELECT * FROM ROL WHERE idRol = ?', [req.params.id],(err, rows)=>{
+            conn.query('SELECT * FROM PERSON WHERE idPerson = ?', [req.params.id],(err, rows)=>{
                         if(err) return res.send(err)
                         res.json(rows)
                     })
@@ -24,7 +24,7 @@ routes.get('/', (req, res) =>{
 routes.post('/', (req, res) =>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
-        conn.query('INSERT INTO ROLE set ?', [req.body], (err, rows)=>{
+        conn.query('INSERT INTO PERSON set ?', [req.body], (err, rows)=>{
                     if(err) return res.send(err)
                     res.json(rows)
                 })
@@ -34,7 +34,7 @@ routes.post('/', (req, res) =>{
 routes.delete('/:id', (req, res) =>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
-        conn.query('DELETE FROM ROLE WHERE idRole = ?', [req.params.id], (err, rows)=>{
+        conn.query('DELETE FROM PERSON WHERE idPerson = ?', [req.params.id], (err, rows)=>{
                     if(err) return res.send(err)
                     res.json(rows)
                 })
@@ -44,7 +44,7 @@ routes.delete('/:id', (req, res) =>{
 routes.put('/:id', (req, res) =>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
-        conn.query('UPDATE ROLE set ? WHERE idRole = ?', [req.body, req.params.id], (err, rows)=>{
+        conn.query('UPDATE PERSON set ? WHERE idPerson = ?', [req.body, req.params.id], (err, rows)=>{
                     if(err) return res.send(err)
                     res.json(rows)
                 })
---
+++ routes/rol_routers.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const routes = express.Router()
+
+routes.get('/', (req, res) =>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('SELECT * FROM ROL', (err, rows)=>{
+                    if(err) return res.send(err)
+                    res.json(rows)
+                })
+    })
+
+    routes.get('/:id', (req, res) =>{
+        req.getConnection((err, conn)=>{
+            if(err) return res.send(err)
+            conn.query('SELECT * FROM ROL WHERE idRol = ?', [req.params.id],(err, rows)=>{
+                        if(err) return res.send(err)
+                        res.json(rows)
+                    })
+        })
+    })
+})
+
+routes.post('/', (req, res) =>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('INSERT INTO ROL set ?', [req.body], (err, rows)=>{
+                    if(err) return res.send(err)
+                    res.json(rows)
+                })
+    })
+})
+
+routes.delete('/:id', (req, res) =>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('DELETE FROM ROL WHERE idRol = ?', [req.params.id], (err, rows)=>{
+                    if(err) return res.send(err)
+                    res.json(rows)
+                })
+    })
+})
+
+routes.put('/:id', (req, res) =>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('UPDATE ROL set ? WHERE idRol = ?', [req.body, req.params.id], (err, rows)=>{
+                    if(err) return res.send(err)
+                    res.json(rows)
+                })
+    })
+})
+
+module.exports =routes;
