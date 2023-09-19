5645ea505002bdf3af7f5ad1fed169f5ef2538af
CharlotteLanguages <admin@charlottelanguagescenter.com>
2023-09-19 11:35:48 -0500

    ajustes conexion

--- dbConnection.js
+++ dbConnection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-var conn = mysql.createPool({
+/*var conn = mysql.createPool({
     host: 'containers-us-west-153.railway.app',
     port: 5905,
     user: 'root',
@@ -9,12 +9,22 @@ var conn = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-}); 
+});*/
 
-/*conn.connect(function(err) {
+var conn = mysql.createConnection({
+  host: 'containers-us-west-153.railway.app',
+  port: 5905,
+  user: 'root',
+  password: '4UuCsu5O6YBY7ZUlSXLr',
+  database: 'railway'
+});
+
+conn.connect(function(err) {
   if (err) throw err;
   console.log('Database is connected successfully !');
-});*/
-module.exports = conn.promise;
+});
+
+//module.exports = conn.promise;
+module.exports = conn;
 
 
