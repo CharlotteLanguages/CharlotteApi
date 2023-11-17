137c132dff1fe829fd0be89c0f78385a8640e313
CharlotteLanguages <admin@charlottelanguagescenter.com>
2023-11-17 11:10:28 -0500

    ajustes

--- routes/sponsor_routers.js
+++ routes/sponsor_routers.js
@@ -10,12 +10,12 @@ const imagesController = require("../controllers/patrocinadorController");
 const { SourceTextModule } = require('vm');
 
 routes.post(
-    "/sponsor/:tabla",
+    "/:tabla",
     imagesController.upload,
     imagesController.uploadFile
 );
 
-routes.get('/sponsor', (req, res) => {
+routes.get('/', (req, res) => {
     req.getConnection((err, conn) => {
         if (err) return res.send(err)
         conn.query('SELECT idPatrocinador, nombre, direccion, webSite, image as imagen_url, nameImage, tipo FROM PATROCINADOR', (err, rows) => {
@@ -25,7 +25,7 @@ routes.get('/sponsor', (req, res) => {
     })
 })
 
-routes.get('/sponsor/:id', (req, res) => {
+routes.get('/:id', (req, res) => {
     req.getConnection((err, conn) => {
       if (err) return res.send(err)
       conn.query('SELECT idPatrocinador, nombre, direccion, webSite, image as imagen_url, nameImage, tipo FROM PATROCINADOR WHERE idPatrocinador = ?', [req.params.id], (err, rows) => {
@@ -43,20 +43,20 @@ routes.get('/sponsor/:nameImage', (req, res) => {
 
     db.query(sql, [id], (err, result) => {
         if(err) {
-          return res.statusMessage(500).send(err);
+            return res.statusMessage(500).send(err);
         }
         if (result.length === 0) {
-          return res.status(404).send('Archivo no encontrado.');
+            return res.status(404).send('Archivo no encontrado.');
         }
         const { tipo, imagenBuffer } = result[0];
-    
+
         res.setHeader('Content-Type', tipo);
-    
+
         res.send(imagenBuffer);
-      })
+    })
 })
 
-routes.delete('/sponsor/:id', (req, res) => {
+routes.delete('/:id', (req, res) => {
     req.getConnection((err, conn) => {
         if (err) return res.send(err)
         conn.query('DELETE FROM PATROCINADOR WHERE idPatrocinador = ?', [req.params.id], (err, rows) => {
